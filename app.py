fbb4f6579d0badae0732937afa6d6cd33c80fa80
Lenovo <@>
2024-03-10 21:43:31 +0300

    добавление и вывод инфо на стр

--- app.py
+++ app.py
@@ -1,31 +1,25 @@
-from flask import Flask, render_template
-from sqlalchemy import SQLalchemy
-import datetime
+from flask import Flask, render_template, request,redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
 
 app = Flask(__name__)
-app.config['SQLAlCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
-db = SQLalchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+db = SQLAlchemy(app)
+app.app_context().push()
 
 
 class Article(db.Model):
-    id = db.Column(db.Intejer, primari_key=True)
-    title = db.Column(db.String(100), nullabl=False)
-    intro = db.Column(db.String(300), nullabl=False)
-    text = db.Column(db.Text, nullabl=False)
-    date = db.Column(db.DataTime, default=datetime.utcnow)
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    intro = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.now())
 
     def __repr__(self):
         return '<Article %r>' % self.id
 
-
-class ArticleMine(db.Model):
-    id = db.Column(db.Intejer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    intro = db.Column(db.String(300), nullable=False)
-    text = db.Column(db.Text, nullable=False)
-    date = db.Column(db.DataTime, default=datetime.datetime)
-    image = db.Column(db.LargeBinary(length=500), nullable=False)
-    grade = db.Column(db.Intejer, nullable=False)
 
 
 @app.route('/')
@@ -38,16 +32,36 @@ def index():
 def about():
     return render_template('about.html')
 
+@app.route('/posts')
+def posts():
+    articles=Article.query.order_by(Article.date).all()
+    return render_template('posts.html', articles=articles)
 
-@app.route('/user/<string:name>/<int:id>')
-def user(name, id):
-    return 'hello' + name + '-' + str(id)
+@app.route('/create-article', methods=['POST', 'GET'])
+def create_article():
+    if request.method == 'POST':
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
+        article=Article(title=title, intro=intro, text=text)
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'При добавлении статью произошла ошибка'
+    else:
+        return render_template('create-article.html')
 
 
-@app.route('/mine')
-def mine():
-    return render_template('mine.html')
+# @app.route('/user/<string:name>/<int:id>')
+# def user(name, id):
+#     return 'hello' + name + '-' + str(id)
+
+
+
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
---
+++ appMine.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+appMine = Flask(__name__)
+appMine.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blogMine.db'
+db = SQLAlchemy(appMine)
+appMine.app_context().push()
+
+
+class ArticleMine(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    intro = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    comment = db.Column(db.Text, nullable=True)
+    date = db.Column(db.DateTime, default=datetime.now())
+
+    def __repr__(self):
+        return '<ArticleMine %r>' % self.id
+
+
+@appMine.route('/')
+def mine():
+    return render_template('mine.html')
+
+
+@appMine.route('/create-mine', methods=['POST', 'GET'])
+def create_mine():
+    if request.method == 'POST':
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
+        comment = request.form['comment']
+        articleMine = ArticleMine(title=title, intro=intro, text=text, comment=comment)
+        try:
+            db.session.add(articleMine)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'При добавлении статью произошла ошибка'
+    else:
+        return render_template('create-mine.html')
+
+
+@appMine.route('/posts-mine')
+def posts_mine():
+    articlesMine = ArticleMine.query.order_by(ArticleMine.date).all()
+    return render_template('posts-mine.html', articlesMine=articlesMine)
+
+
+if __name__ == '__main__':
+    appMine.run(debug=True)
